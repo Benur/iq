e5da18a14bce481c8e252267e3e1531abe883c17
Ruben Schmidt <ruben.schmidt@tum.de>
2020-10-03 18:13:16 +0200

    csv reading and converting values to float

---
+++ task1/Readme.md
@@ -0,0 +1,11 @@
+# TODOs:
+- read csv file
+    + String
+    + Date
+    + Time
+    + float
+    + int
+- handle missing values
+- optionally normalize data
+- lin reg with grad desc alg
+- make qt application
--- task1/main.cpp
+++ task1/main.cpp
@@ -1,24 +1,82 @@
+//#include <dataviewer.h>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <QApplication>
-#include <QPushButton>
-#include <dlib/algs.h>
-
-using namespace dlib;
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+ 
 using namespace std;
 
+const string filename = "AirQualityUCI.csv";
+
+vector<pair<string, vector<float>>> read_csv(string filename) {
+    vector<pair<string,vector<float>>> result;
+
+    ifstream myFile(filename);
+    if (!myFile.is_open()) throw runtime_error("Could not open file");
+    string line, colname, val, year, month, day;
+
+    if(myFile.good())
+    {
+        getline(myFile, line);
+        stringstream ss(line);
+        while(getline(ss, colname, ';')) {
+            if (colname.empty()) break;
+            result.push_back({colname, vector<float> {}});
+        }
+    }
+
+    while(getline(myFile, line) && line.substr(0,1) != ";")
+    {
+        stringstream ss(line);
+        int colIdx = 0;
+        while (getline(ss, val, ';') && colIdx < result.size())
+        {
+           if (colIdx == 0) {
+               day = val.substr(0,2);
+               month = val.substr(3,2);
+               year = val.substr(6,4);
+               val = year + month + day;
+           }
+            replace(val.begin(), val.end(), ',', '.');
+            result.at(colIdx).second.push_back(stof(val));
+            if(ss.peek() == ';') ss.ignore();
+            colIdx++;
+
+        }
+        
+    }
+    myFile.close();
+    return result;
+}
 
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
+    QApplication DataViewer(argc, argv);
+    
+    const string filename = "AirQualityUCI.csv";
+    // read data
+    vector<pair<string, vector<float>>> data = read_csv(filename);
+    
+    // handle missing data
 
- QPushButton button1 ("test");
- QPushButton button2 ("other", &button1);
- unsigned long x = 9;
- cout << square_root(x) << endl;
+    // normalize data
 
- button1.show();
+    // lin reg
 
- return app.exec();
+    /*if (__cplusplus == 201703L) std::cout << "C++17\n";
+    else if (__cplusplus == 201402L) std::cout << "C++14\n";
+    else if (__cplusplus == 201103L) std::cout << "C++11\n";
+    else if (__cplusplus == 199711L) std::cout << "C++98\n";
+    else std::cout << "pre-standard C++\n";
+    char *path=NULL;
+    size_t size;
+    path=getcwd(path,size);
+    cout<<"\n current Path: "<<path<<"\n";*/
+
+    cout << data.at(1).second.at(29) << "\n";
+    return 0;
 }
